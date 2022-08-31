41f26785d12d024f3b150957718b7d94be31770c
NasrinR791 <nasein.rahimie@gmail.com>
2022-08-31 17:23:56 +0300

    version 2

--- s001.py
+++ s001.py
@@ -1,8 +1,31 @@
-Upper_limit = 1000
-Sum = 0
+import time
 
-for i in range(Upper_limit):
-	if i % 3 ==0 or i % 5 == 0:
-		Sum += i
 
-print(Sum)
+def method1():
+	start1 = time.time()
+	Upper_limit = 1000
+	Sum = 0
+	for i in range(Upper_limit):
+		if i % 3 ==0 or i % 5 == 0:
+			Sum += i
+	stop1 = time.time()
+	comp_time= stop1 - start1
+	return Sum , comp_time
+
+
+
+def method2():
+	start2 = time.time()
+	Sum = sum(i for i in range(1000) if i % 3==0 or i% 5 == 0)
+	stop2 = time.time()
+	comp_time = stop2-start2
+	return Sum , comp_time
+
+
+def multiples_sum():
+	answer = sum(i for i in range(1000) if i % 3==0 or i% 5 == 0)
+	return answer
+
+
+if __name__=="__main__":
+	print(multiples_sum())
